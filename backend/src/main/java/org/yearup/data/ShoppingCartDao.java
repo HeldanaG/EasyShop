f669c5ce229f40377d1cd9628d5bf86383eea442
HeldanaG <heldanagmariam@gmail.com>
2025-06-25 13:08:18 -0500

    Added signature mathods in ShoppingCartDao

--- backend/src/main/java/org/yearup/data/ShoppingCartDao.java
+++ backend/src/main/java/org/yearup/data/ShoppingCartDao.java
@@ -2,8 +2,20 @@ package org.yearup.data;
 
 import org.yearup.models.ShoppingCart;
 
-public interface ShoppingCartDao
+public interface    ShoppingCartDao
 {
+
+    // 1. Get current user's cart with all items and total
     ShoppingCart getByUserId(int userId);
+
     // add additional method signatures here
+
+    // 2. Add a product OR increment its quantity by 1
+    void addOrIncrement(int userId, int productId);
+
+    // 3. Set quantity for a product
+    void setQuantity(int userId, int productId, int quantity);
+
+    // 4. Clear the user's entire cart
+    void clear(int userId);
 }
